b9f1ae9bb5d47e7472f2f1fbb05598e914b3a0b5
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-27 20:38:49 -0300

    chore content handling

--- terraform/api_gw_proxy_cors.tf
+++ terraform/api_gw_proxy_cors.tf
@@ -25,10 +25,11 @@ resource "aws_api_gateway_method_response" "options" {
 }
 
 resource "aws_api_gateway_integration" "options" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.options.http_method
-  type        = "MOCK"
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.proxy.id
+  http_method      = aws_api_gateway_method.options.http_method
+  type             = "MOCK"
+  content_handling = "CONVERT_TO_TEXT"
 }
 
 resource "aws_api_gateway_integration_response" "options" {
