edc0b30db1400f302877a91b9f8a69937ab03459
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-29 18:08:40 -0300

    policy

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -17,8 +17,7 @@ resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
   policy = jsonencode({
-    "Version" : "2008-10-17",
-    "Id" : "PolicyForCloudFrontPrivateContent",
+    "Version" : "2012-10-17",
     "Statement" : [
       {
         "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
@@ -33,9 +32,30 @@ resource "aws_s3_bucket_policy" "website" {
             "AWS:SourceArn" : aws_cloudfront_distribution.website.arn
           }
         }
+      },
+      {
+        "Sid" : "AllowLegacyOAIReadOnly",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.website.id}"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.website.arn}/*",
       }
     ]
   })
 
   depends_on = [aws_s3_bucket_public_access_block.website]
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
 }
