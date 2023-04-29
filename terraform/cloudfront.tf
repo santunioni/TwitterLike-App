51c4b9b660ff8f74a906c250ca155c7b5c711190
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-29 15:13:39 -0300

    chore: api gateway cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,48 +1,9 @@
 resource "aws_cloudfront_distribution" "website" {
-
-  origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.website.id
-  }
-
   enabled             = true
   is_ipv6_enabled     = true
   comment             = local.NAME
   price_class         = "PriceClass_100"
   default_root_object = "index.html"
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.website.id
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-  }
-
-  ordered_cache_behavior {
-    path_pattern           = "/api/*"
-    allowed_methods        = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"]
-    cached_methods         = ["GET"]
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-    cache_policy_id        = ""
-
-    forwarded_values {
-      query_string = true
-      headers      = ["Authorization"]
-      cookies {
-        forward = "none"
-      }
-    }
-    target_origin_id = aws_api_gateway_rest_api.api.execution_arn
-  }
 
   restrictions {
     geo_restriction {
@@ -56,6 +17,58 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   tags = local.COMMON_TAGS
+
+  origin {
+    domain_name = replace(aws_api_gateway_deployment.stage_v1.invoke_url, "/^https?://([^/]*).*/", "$1")
+    origin_id   = "apigw"
+    origin_path = aws_api_gateway_deployment.stage_v1.stage_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "apigw"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.website.id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+  }
 }
 
 resource "aws_cloudfront_cache_policy" "api_cache" {
