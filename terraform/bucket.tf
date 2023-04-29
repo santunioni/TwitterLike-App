0b4bbb2d0c49649fd94c3c191b0b5f594ee446c8
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-29 15:31:05 -0300

    chore: restrict bucket policy

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -18,14 +18,20 @@ resource "aws_s3_bucket_policy" "website" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect : "Allow"
-        Principal : "*"
-        Action : "s3:GetObject"
-        Resource : "${aws_s3_bucket.website.arn}/*"
+    "Statement" : {
+      "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : "cloudfront.amazonaws.com"
+      },
+      "Action" : "s3:GetObject",
+      "Resource" : "${aws_s3_bucket.website.arn}/*",
+      "Condition" : {
+        "StringEquals" : {
+          "AWS:SourceArn" : aws_cloudfront_distribution.website.arn
+        }
       }
-    ]
+    }
   })
 
   depends_on = [aws_s3_bucket_public_access_block.website]
