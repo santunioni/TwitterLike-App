bb681e4d6d01ea51e2d8f280c01baab4ace433e3
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-29 21:09:49 -0300

    feat: implement signup page

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,12 +1,20 @@
+import { getCurrentUser } from '@packages/client/src/utils/trpc'
 import Link from 'next/link'
 
 export default function Home() {
+  const isLogged = getCurrentUser() !== null
   return (
     <>
       <Link href="/feed">Feed</Link>
       <Link href="/search">Search</Link>
-      <Link href="/login">Login</Link>
-      <Link href="/me">Me</Link>
+      {isLogged ? (
+        <Link href="/me">Me</Link>
+      ) : (
+        <>
+          <Link href="/login">Login</Link>
+          <Link href={'/signup'}>Signup</Link>
+        </>
+      )}
     </>
   )
 }
---
+++ packages/client/src/pages/signup.tsx
@@ -0,0 +1,69 @@
+import { getApiBaseUrl, getCurrentUser, setGlobalToken } from '@packages/client/src/utils/trpc'
+import Link from 'next/link'
+import { useState } from 'react'
+import { z } from 'zod'
+
+const zodEmail = z.string().email()
+
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(getCurrentUser())
+
+  const isInvalidEmail = Boolean(email) && !zodEmail.safeParse(email).success
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const fetchReturn = await fetch(`${getApiBaseUrl()}/api/accounts/signup`, {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        user: {
+          email,
+          password,
+        },
+      }),
+    })
+    const { access_token } = await fetchReturn.json()
+    if (fetchReturn.status === 401) {
+      window.alert('Invalid credentials')
+      return
+    }
+    if (!fetchReturn.ok) {
+      window.alert('Something went wront :(')
+      return
+    }
+    setGlobalToken(access_token)
+    setUser(getCurrentUser())
+  }
+
+  if (user) {
+    return (
+      <>
+        <div>Logged as {user.email}</div>
+        <button onClick={() => window.history.go(-1)}>Back</button>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Link href={'/'}>Início</Link>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Username:</label>
+            <input type="text" id="email" value={email} onChange={event => setEmail(event.target.value)} />
+            <>{isInvalidEmail && 'Invalid Email'}</>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" value={password} onChange={event => setPassword(event.target.value)} />
+          </div>
+          <button onClick={() => window.history.go(-1)}>Back</button>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  )
+}
